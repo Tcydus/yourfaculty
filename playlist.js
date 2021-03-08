c3a349490169f2cc3948db3c3c568dc6ba5e470f
RaViS888 <68078036+RaViS888@users.noreply.github.com>
2021-03-08 22:03:17 +0700

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,20 @@
+document.getElementById("btn1TCAS").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3fuyd1ugjjCtImj1NNFKYUS";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "TCAS";
+};
+document.getElementById("btn2ONET").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com//embed/videoseries?list=PLEb4MZumkF3fPAXNaHMLilgl0hjkeiJsI";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "ONET";
+};
+document.getElementById("btn3GatPat").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3dwQNj8zFWUI6QBq53xaIlV";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "GAT/PAT";
+};
+document.getElementById("btn4_9").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3fgAR01Sw1eq4bwy9bPsPeo";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "9วิชาสามัญ";
+};
