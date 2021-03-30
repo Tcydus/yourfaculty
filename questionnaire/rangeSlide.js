2f4aeb7785c0226a687556fe28b89b9ca6ab1609
tcydus <natthanan43@hotmail.com>
2021-03-30 07:41:58 +0700

    add offset questionaire

--- questionnaire/rangeSlide.js
+++ questionnaire/rangeSlide.js
@@ -1,4 +1,5 @@
 var inputValList = [];
+var tempValList = [];
 const position = "beforeend";
 
 var data = [
@@ -43,17 +44,18 @@ var data = [
 
 function addHTMLSubjuct(){
   const list = document.getElementById("rangeInput-field");
-  var subjectList = ["คณิตศาสตร์","ภาษาอังกฤษ","","","","","",""]
+
   
-  subjectList.forEach(function(subject,index){
-    dictData = data[0].values[index];
+  data[0].values.forEach(function(value,index){
+    dictData = data[0].values[index]; // equivalent value parameter
     dictData.point = 0;
+    inputValList[index] = 0
     const text = `<div class="range">
                     <div class="slider-value">
                       <span class="span-show"></span>
                     </div>
                     <div class="field">
-                      <div class="value text">${dictData.subject}</div>
+                      <div class="value text">${value.subject}</div>
                       <div class="value left">0</div>
                       <input type="range" min="0" max="5" value="0" steps="1"> 
                       <div class="value right">5</div>
@@ -63,8 +65,8 @@ function addHTMLSubjuct(){
     list.insertAdjacentHTML(position,text);
    
   })
-  
-  console.log(data[0].values);
+  console.log(inputValList)
+  // console.log(data[0].values);
 }
 function addInputRange(){
     const slideValue = document.querySelectorAll("span.span-show");
@@ -81,7 +83,8 @@ function addInputRange(){
 
       dictData = data[0].values[index];
       dictData.point = Number(value);
-      
+      inputValList[index] =  Number(value);
+
       slideValue[index].textContent = value;
       showValue[index].innerHTML =value;
   
@@ -89,7 +92,7 @@ function addInputRange(){
       const newPosition = 10 - (newValue * 0.2)
       slideValue[index].style.left = `calc(${newValue}% + (${newPosition}px))`;
       slideValue[index].classList.add("show");
-      console.log(data[0].values);
+      // console.log(data[0].values);
       RadarChart(".radarChart", data, Option1);
     };
     eachSlider.onblur = (() =>{
@@ -99,6 +102,101 @@ function addInputRange(){
   })
   
 }
+
+var offsetResult = 2;
+function checkInputVal(x0,x1,x2,x3,x4,x5,x6,x7){
+  let y = inputValList;
+  return (y[0] == x0 && y[1] == x1 && y[2] == x2 && y[3] == x3 && 
+          y[4] == x4 && y[5] == x5 && y[6] == x6 && y[7] == x7)
+}
+function checkOffsetDiff(x0,x1,x2,x3,x4,x5,x6,x7){
+  let y = inputValList;
+  let v = offsetResult;
+  return (Math.abs(y[0] - x0) <= v  &&  Math.abs(y[1] - x1) <= v  &&  Math.abs(y[2] - x2) <= v && 
+          Math.abs(y[3] - x3) <= v  &&  Math.abs(y[4] - x4) <= v  && Math.abs(y[5] - x5) <= v  &&
+          Math.abs(y[6] - x6) <= v  &&  Math.abs(y[7] - x7) <= v )
+}
+
+
+
+function analysisLocation(){
+ 
+
+
+    // for(let i = 0; i<= 5; i++){
+    //   inputValList[7] = i;
+    //   for(let j = 0; j<= 5; j++){
+    //     inputValList[6] = j;
+    //     for(let k = 0; k<= 5; k++){
+    //       inputValList[5] = k;
+    //       for(let l = 0; l<= 5; l++){
+    //         inputValList[4] = l;
+    //         for(let m = 0; m<= 5; m++){
+    //           inputValList[3] = m;
+    //           for(let n = 0; n<= 5; n++){
+    //             inputValList[2] = n;
+    //             for(let o = 0; o<= 5; o++){
+    //               inputValList[1] = o;
+    //               for(let p = 0; p<= 5; p++){
+    //                 inputValList[0] = p;
+    //                 console.log(inputValList)
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+  
+  // }
+
+  // inputValList.forEach(function(value,i){
+  //   inputValList[i] =  Math.floor(Math.random() * 6);
+  // });
+  // tempValList.forEach(function(value,i) {
+  //   // if(value != inputValList){
+  //   //   tempValList.push(inputValList)
+  //   // }
+  // });
+  // console.log(tempValList.length);
+  // if(tempValList.length == 0){
+  //   tempValList.push(inputValList);
+  //   }
+
+  // console.log(inputValList);
+  // console.log(tempValList);
+
+  let resultLocation = "none.html";
+  if(checkOffsetDiff(5,2,5,3,2,4,2,3)){
+    resultLocation = "engineer.html";
+  }
+  else if(checkOffsetDiff(3,5,2,4,2,3,1,2)){
+    resultLocation = "doctor.html";  
+  }
+  else if(checkOffsetDiff(4,0,0,0,5,5,3,5)){
+    resultLocation = "business.html";
+  }
+  else if(checkOffsetDiff(5,5,5,5,4,5,3,4)){
+    resultLocation = "scientist.html";
+  }
+  else if(checkOffsetDiff(0,0,0,0,5,5,5,5)){
+    resultLocation = "human.html";
+  }
+  else if(checkOffsetDiff(2,0,0,0,4,4,0,5)){
+    resultLocation = "lawyer.html";
+  }
+  else if(checkOffsetDiff(2,0,0,0,5,2,5,5)){
+    resultLocation = "art.html";
+  }
+  // console.log(inputValList)
+  // console.log(resultLocation)
+  // resultLocation = "engineer.html";
+  // resultLocation = "result.html"
+  window.location.replace(resultLocation)
+}
+
+
 addHTMLSubjuct();
 addInputRange();
 
