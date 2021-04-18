b6472692d91cbb35255fece6366625d3a54df087
tcydus <natthanan43@hotmail.com>
2021-04-19 01:22:02 +0700

    edit homepage

---
+++ Home/video-display.js
@@ -0,0 +1,20 @@
+document.getElementById("button1").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3fuyd1ugjjCtImj1NNFKYUS";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "TCAS";
+};
+document.getElementById("button2").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com//embed/videoseries?list=PLEb4MZumkF3fPAXNaHMLilgl0hjkeiJsI";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "ONET";
+};
+document.getElementById("button3").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3dwQNj8zFWUI6QBq53xaIlV";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "GAT/PAT";
+};
+document.getElementById("button4").onclick = function() {
+    document.getElementById('playlist').src = "https://youtube.com/embed/videoseries?list=PLEb4MZumkF3fgAR01Sw1eq4bwy9bPsPeo";
+    var x = document.getElementById("playnow");
+     x.innerHTML = "9วิชาสามัญ";
+};
